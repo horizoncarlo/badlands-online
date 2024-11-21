be111502663c982dc1525d6e3fc218aa22e1014e
Cruz Miraback <inset-tackler0v@icloud.com>
2024-11-21 09:13:43 -0700

    Typescript things

--- backendjs/scraper.ts
+++ backendjs/scraper.ts
@@ -1,4 +1,4 @@
-import { chromium } from 'npm:playwright';
+import { Browser, chromium } from 'npm:playwright';
 import rua from 'https://deno.land/x/rua/mod.js';
 
 let scraperRunning = false;
@@ -11,7 +11,7 @@ export function startScraper() {
   scrapeCardData().then(() => {
     console.log('Card scraper done');
   });
-  return 'Started card scaper';
+  return 'Started card scraper';
 }
 
 const scrapeCardData = async () => {
@@ -19,8 +19,9 @@ const scrapeCardData = async () => {
   try {
     console.log('Launching browser...');
     browser = await chromium.launch();
-    await getCards(browser, 'People');
-    await getCards(browser, 'Camp');
+    const baseUrl = 'https://radlands.fandom.com/wiki';
+    await getCards(browser, baseUrl, 'People');
+    await getCards(browser, baseUrl, 'Camp');
   } catch (e) {
     console.error(`Encountered unexpected error: ${e}`);
   } finally {
@@ -29,27 +30,27 @@ const scrapeCardData = async () => {
   }
 };
 
-const getCards = async (browser, cardType) => {
+const getCards = async (browser: Browser, baseUrl: string, cardType: string) => {
   const context = await browser.newContext({ userAgent: rua() });
-  const baseUrl = 'https://radlands.fandom.com/wiki';
   const cardUrl = `${baseUrl}/${cardType}_Cards`;
   const page = await context.newPage();
   await page.goto(cardUrl, { waitUntil: 'domcontentloaded', timeout: 60000 });
-  const rows = await page.locator('table.fandom-table tbody tr');
+  const rows = page.locator('table.fandom-table tbody tr');
   const rowCount = await rows.count();
   console.log(`Found ${rowCount} ${cardType} cards`);
   const urls = [];
   for (let i = 0; i < rowCount; i++) {
-    const firstTd = rows.nth(i).locator('td:first-child');
-    const anchor = firstTd.locator('a');
-    const href = await anchor.getAttribute('href');
-    urls.push(baseUrl.replace('/wiki', href));
+    const anchor = rows.nth(i).locator('td:first-child').locator('a');
+    const href = await (anchor.getAttribute('href') as unknown as Promise<string|null>);
+    if (href) {
+      urls.push(baseUrl.replace('/wiki', href));
+    }
   }
   context.close();
-  await downloadCardImgs(browser, cardType, urls);
+  await downloadCardImages(browser, cardType, urls);
 };
 
-const downloadCardImgs = async (browser, cardType, urls) => {
+const downloadCardImages = async (browser: Browser, cardType: string, urls: string[]) => {
   console.log(`Downloading ${cardType} images...`);
   const context = await browser.newContext({ userAgent: rua() });
   const pagePromises = urls.map(async (url) => {
@@ -59,20 +60,22 @@ const downloadCardImgs = async (browser, cardType, urls) => {
   const pages = await Promise.all(pagePromises);
   const imgSrcMatch = `${cardType}_-_`;
   for (const page of pages) {
-    const img = await page.locator(`img[src*="${imgSrcMatch}"]`);
-    let imgSrc = await img.first().getAttribute('src');
-    imgSrc = imgSrc.split('.png')[0] + '.png';
-    console.log(`Downloading ${cardType} img ${imgSrc}...`);
-    const response = await fetch(imgSrc);
-    if (response.ok) {
-      const imageBuffer = await response.arrayBuffer();
-      const nameMatch = imgSrc.match(new RegExp(`${imgSrcMatch}([^/]+)\.png`));
-      const name = nameMatch ? nameMatch[1].toLowerCase() : `unknown-${new Date().toISOString()}`;
-      const dir = `cards/${cardType === 'Camp' ? 'camps' : cardType.toLowerCase()}`;
-      await Deno.mkdir(dir, { recursive: true });
-      await Deno.writeFile(`${dir}/${name}.png`, new Uint8Array(imageBuffer));
-    } else {
-      console.warn(`Failed to fetch image: ${response.statusText}`);
+    const img = page.locator(`img[src*="${imgSrcMatch}"]`);
+    let imgSrc = await (img.first().getAttribute('src') as unknown as Promise<string|null>)
+    if (imgSrc) {
+      imgSrc = imgSrc.split('.png')[0] + '.png';
+      console.log(`Downloading ${cardType} img ${imgSrc}...`);
+      const response = await fetch(imgSrc);
+      if (response.ok) {
+        const imageBuffer = await response.arrayBuffer();
+        const nameMatch = imgSrc.match(new RegExp(`${imgSrcMatch}([^/]+)\.png`));
+        const name = nameMatch ? nameMatch[1].toLowerCase() : `unknown-${new Date().toISOString()}`;
+        const dir = `cards/${cardType === 'Camp' ? 'camps' : cardType.toLowerCase()}`;
+        await Deno.mkdir(dir, { recursive: true });
+        await Deno.writeFile(`${dir}/${name}.png`, new Uint8Array(imageBuffer));
+      } else {
+        console.warn(`Failed to fetch image: ${response.statusText}`);
+      }
     }
   }
 };
